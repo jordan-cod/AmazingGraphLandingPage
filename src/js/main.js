fd72dee163e1313e32904a944cc4ada0ffb71013
Gabriel Jordan <gabrijordan3@gmail.com>
2021-06-29 20:54:53 -0300

    media query adicionado, menu mobile adicionado, script para o menu mobile adicionado, modificação no html, diversas modificações no css e font awesome adicionado via cdn

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+function Open() {
+    var menu = document.getElementById("new-menu")
+    menu.classList.add("open")
+}
+function Close() {
+    var menu = document.getElementById("new-menu")
+    menu.classList.remove("open")
+}
+
